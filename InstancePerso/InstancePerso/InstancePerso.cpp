7992b78326a443b3903aa0cc64747c381f616212
Nova3324 <118186038+Nova3324@users.noreply.github.com>
2023-02-01 12:10:49 +0100

    add folder .h in main
    add folder

--- InstancePerso/InstancePerso/InstancePerso.cpp
+++ InstancePerso/InstancePerso/InstancePerso.cpp
@@ -3,9 +3,14 @@
 #include <SFML/Graphics.hpp>
 #include <SFPhysics.h>
 
+#include "Bricks.h"
+#include "Player.h"
+
 int main()
 {
-    // création de la fenêtre
+    Bricks b;
+    Player p;
+    // window creation
     sf::RenderWindow window(sf::VideoMode(1920, 1080), "CodVenture");
 
     // Game while open
@@ -13,13 +18,16 @@ int main()
     {
         sf::Event event;
 
+        b.DisplayBricks();
+        p.CreatePlayer();
+
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
                 window.close();
             if (event.type == sf::Event::MouseButtonPressed)
             {
-
+                b.systemBricks(window, event);
             }
         }
 
@@ -27,7 +35,9 @@ int main()
         window.clear(sf::Color(0, 0, 147));
 
         // Update
-        // window.draw(...);
+        // window.draw(...);        
+        b.DrawSystemBricks(window);
+        p.DrawPlayer(window);
 
         // Last Frame
         window.display();
